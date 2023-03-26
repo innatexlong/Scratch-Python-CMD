a26851f9f0e19f9ce4be0dbc21e7273445062088
innatexlong <103299905+innatexlong@users.noreply.github.com>
2023-03-26 20:58:29 +0800

    0.0.1-23y03 Update

--- class3.py
+++ class3.py
@@ -2,14 +2,22 @@ import time
 
 
 def lang(lt, t):
-    ls = {'tips.PasteSave': {'zh_CN': '请粘贴存档码 >> ', 'en_US': 'Paste Your Save Code Here >> '},
+    ls = {'tip.PasteSave': {'zh_CN': '请粘贴存档码 >> ', 'en_US': 'Paste Your Save Code Here >> '},
           'ask.immeJoin': {'zh_CN': '直接开始编程吗(y/n)', 'en_US': 'Immediate Start Programming?(y/n)'},
           'ask.lang': {'zh_CN': '请选择语言', 'en_US': 'Please Select a Language'},
-          'tips.langList': {'zh_CN': '中文 English', 'en_US': '中文 English'},
+          'tip.langList': {'zh_CN': '中文 English', 'en_US': '中文 English'},
           'act.run': {'zh_CN': '运行程序', 'en_US': 'Run Program'},
+          'tip.noCode': {'zh_CN': '没有代码可以运行', 'en_US': 'No Code Can Run'},
+          'warn.missPara': {'zh_CN': '警告:参数缺失', 'en_US': 'Warning: Missing Parameter'},
+          'weak.tooManyPara': {'zh_CN': '弱警告: 参数数量超过代码', 'en_US': 'Weak Warning: Too Many Parameter'},
+          'fix.addPara': {'zh_CN': '参数数量恢复正常', 'en_US': 'fix.addPara'},
           'temp': {'zh_CN': '', 'en_US': ''}, }
-    lb = ls[lt]
-    return lb[t]
+    if lt in ls:
+        lb = ls[lt]
+        return lb[t]
+    else:
+        lb = lt
+        return lb
 
 
 def i(s):
@@ -26,19 +34,22 @@ def i(s):
 
 
 print(lang('ask.lang', 'zh_CN'))
-la = input(lang('tips.langList', 'zh_CN')+"\n")
+lang_ = ""
+la = input(lang('tip.langList', 'zh_CN')+"\n")
 if la == '中文':
-    l = 'zh_CN'
+    lang_ = 'zh_CN'
 elif la == 'English':
-    l = 'en_US'
+    lang_ = 'en_US'
 else:
-    print('Unknown Language \'%s\'\nProgram Will Exit' % la)
+    print('Unknown Language \'%s\'\n. Program Will Exit after 3 second' % la)
+    print('未知语言 \'%s\'\n。程序将在3秒后退出' % la)
+    time.sleep(3)
     exit(-2)
 
 n, temp = 0, ""
 code, var, v = [], {}, []
 
-save = input(lang('tips.PasteSave', 'zh_CN'))
+save = input(lang('tip.PasteSave', lang_))
 b = False
 for _ in save:
     if save[2*n:2*n+2] == "||":
@@ -59,16 +70,29 @@ for _ in save:
             if n >= len(save)-1:
                 break
     n += 1
-n = 0
+if len(code) >> len(v):
+    print(lang('warn.missPara', lang_))
+    for _ in range(len(code)-len(v)+1):
+        v.append(0)
+    print(lang('fix.addPara', lang_))
+elif len(code) << len(v):
+    print(lang('weak.tooManyPara', lang_))
+else:
+    pass
 
 
 def run(nc, c):
     if str(c) == '00':
-        print(v[nc])
+        print(v[int(nc)])
     if str(c) == '01':
-        time.sleep(v[nc])
+        time.sleep(v[int(nc)])
 
 
-if input(lang('act.run', l)):
-    for n in save:
-        run(int(n), code[int(n)])
+if input(lang('act.run', lang_)) == "Run Program":
+    if len(code) >> 0:
+        n = 0
+        for _ in save:
+            run(int(n), code[int(n)])
+            n += 1
+    else:
+        print(lang('tip.noCode', lang_))
