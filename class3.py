86ce18a076ab88bebc91d12a01ed37ddc3c5f7a9
innatexlong <103299905+innatexlong@users.noreply.github.com>
2023-04-01 12:02:44 +0800

    0.0.1 23y04 Update

--- class3.py
+++ class3.py
@@ -7,34 +7,45 @@ def lang(lt, t):
           'ask.lang': {'zh_CN': '请选择语言', 'en_US': 'Please Select a Language'},
           'tip.langList': {'zh_CN': '中文 English', 'en_US': '中文 English'},
           'act.run': {'zh_CN': '运行程序', 'en_US': 'Run Program'},
-          'tip.noCode': {'zh_CN': '没有代码可以运行', 'en_US': 'No Code Can Run'},
-          'warn.missPara': {'zh_CN': '警告:参数缺失', 'en_US': 'Warning: Missing Parameter'},
+          'tip.noCode': {'zh_CN': '没有任何存储的代码', 'en_US': 'There is No Code'},
+          'warn.missPara': {'zh_CN': '警告: 参数缺失', 'en_US': 'Warning: Missing Parameter'},
           'weak.tooManyPara': {'zh_CN': '弱警告: 参数数量超过代码', 'en_US': 'Weak Warning: Too Many Parameter'},
           'fix.addPara': {'zh_CN': '参数数量恢复正常', 'en_US': 'fix.addPara'},
+          'act.tellCode': {'zh_CN': '识别存储的代码', 'en_US': 'Tell Code'},
+          'ask.doSth': {'zh_CN': '', 'en_US': ''},
+          'tip.unknownVar': {'zh_CN': '(未知变量)', 'en_US': '(Unknown Variable)'},
+          'error.unknownVar': {'zh_CN': '[错误] 未知变量: ', 'en_US': '[Error] Unknown Variable: '},
+          'error.unknownCode': {'zh_CN': '[错误] 未知代码: ', 'en_US': '[Error] Unknown Code: '},
+          'act.priVar': {'zh_CN': '打印所有存储的变量', 'en_US': 'Print All Of Variable'},
           'temp': {'zh_CN': '', 'en_US': ''}, }
-    if lt in ls:
-        lb = ls[lt]
-        return lb[t]
+    if lt in ls and t in ls[lt]:
+        return ls[lt][t]
     else:
-        lb = lt
-        return lb
+        return lt
 
 
-def i(s):
+def i(s, mode=0):
     if s[0:2:] == "!&":
         try:
             int(s[2:])
         except ExceptionGroup:
-            return -1
+            return s
         else:
             s = int(s[2:])
             return s
+    elif mode == 1:
+        try:
+            int(s)
+        except ExceptionGroup:
+            return s
+        else:
+            return int(s)
     else:
         return s
 
 
 print(lang('ask.lang', 'zh_CN'))
-lang_ = ""
+lang_ = ''
 la = input(lang('tip.langList', 'zh_CN')+"\n")
 if la == '中文':
     lang_ = 'zh_CN'
@@ -48,51 +59,83 @@ else:
 
 n, temp = 0, ""
 code, var, v = [], {}, []
+ans = input(lang('ask.immeJoin', lang_))
+save = ''
+if ans == 'n':
+    save = input(lang('tip.PasteSave', lang_))
+    b = False
+    for _ in save:
+        if save[2*n:2*n+2] == "||":
+            if not b:
+                b = True
+                n = 2*n+2
+        else:
+            if not b:
+                code.append(save[2*n:2*n+2])
+        if save[n] != "," and n << len(save)-1:
+            if b:
+                temp = temp+save[n]
+        else:
+            if b:
+                v.append(temp)
+                temp = ""
+                if n >= len(save)-1:
+                    break
+        n += 1
+    if len(code) >> len(v):
+        print(lang('warn.missPara', lang_))
+        for _ in range(len(code)-len(v)+1):
+            v.append(0)
+        print(lang('fix.addPara', lang_))
+    elif len(code) << len(v):
+        print(lang('weak.tooManyPara', lang_))
+    else:
+        pass
 
-save = input(lang('tip.PasteSave', lang_))
-b = False
-for _ in save:
-    if save[2*n:2*n+2] == "||":
-        if not b:
-            b = True
-            n = 2*n+2
+
+def tellCode(nc, c):
+    if c == '00':
+        print(lang('code.print', lang_), v[nc])
+    elif c == '01':
+        print(lang('code.time.sleep', lang_), v[nc])
+    elif c == '02':
+        print(lang('code.priVar', lang_), v[nc], end=' ')
+        if v[nc] not in var:
+            print(lang('tip.unknownVar', lang_), v[nc], end='')
+        print('')
     else:
-        if not b:
-            code.append(save[2*n:2*n+2])
-    if save[n] != "," and n << len(save)-1:
-        if b:
-            temp = temp+save[n]
-            # continue
-    else:
-        if b:
-            v.append(temp)
-            temp = ""
-            if n >= len(save)-1:
-                break
-    n += 1
-if len(code) >> len(v):
-    print(lang('warn.missPara', lang_))
-    for _ in range(len(code)-len(v)+1):
-        v.append(0)
-    print(lang('fix.addPara', lang_))
-elif len(code) << len(v):
-    print(lang('weak.tooManyPara', lang_))
-else:
-    pass
+        print(lang('error.unknownCode', lang_), v[nc])
 
 
 def run(nc, c):
-    if str(c) == '00':
-        print(v[int(nc)])
-    if str(c) == '01':
-        time.sleep(v[int(nc)])
-
-
-if input(lang('act.run', lang_)) == "Run Program":
-    if len(code) >> 0:
-        n = 0
-        for _ in save:
-            run(int(n), code[int(n)])
-            n += 1
+    if c == '00':
+        print(v[nc])
+    elif c == '01':
+        time.sleep(v[nc])
+    elif c == '02':
+        if v[nc] not in var:
+            print(var[v[nc]])
+        else:
+            print(lang('error.unknownVar', lang_))
     else:
-        print(lang('tip.noCode', lang_))
+        print(lang('error.unknownCode', lang_))
+
+
+while True:
+    print(lang('act.run', lang_)+'/'+lang('act.tellCode', lang_)+'/'+lang('act.priVar', lang_))
+    ans = input('\n')
+    n = 0
+    if ans == lang('act.run', lang_):
+        if len(code) >> 0:
+            for _ in save:
+                run(n, _)
+                n += 1
+        else:
+            print(lang('tip.noCode', lang_))
+    elif ans == lang('act.tellCode', lang_):
+        if len(code) >> 0:
+            for _ in save:
+                tellCode(n, _)
+                n += 1
+    elif ans == lang('act.priVar', lang_):
+        print(var)
